e9849d932d180128ba3799d4a797f36180e76460
Abhishek <abhishekphd14@gmail.com>
2022-04-27 16:10:04 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:chatter/Screens/screens.dart';
+import 'package:chatter/theme.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(const MyApp());
@@ -9,6 +10,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // ignore: dead_code
-    return MaterialApp(title: 'weChat', home: HomeScreen());
+    return MaterialApp(
+        theme: AppTheme.light(),
+        darkTheme: AppTheme.dark(),
+        themeMode: thememode.dark,
+        title: 'weChat',
+        home: const HomeScreen());
   }
 }
